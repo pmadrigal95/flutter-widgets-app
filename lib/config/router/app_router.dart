9904a24f608deec6154cecbefbac794400e43a5f
Pablo Madrigal <pablomadrigal.1995@gmail.com>
2023-07-30 12:56:02 -0600

    Go router with name and animation example

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,23 +1,48 @@
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:widgets_app/presentation/screens/screens.dart';
 
 // GoRouter configuration
 final appRouter = GoRouter(
-  initialLocation: '/', // Opcional arranca con esta pantalla, si no existe esta linea inicia con la primera del routes
+  initialLocation:
+      '/', // Opcional arranca con esta pantalla, si no existe esta linea inicia con la primera del routes
   routes: [
     GoRoute(
       path: '/',
+      name: HomeScreen.name,
       builder: (context, state) => const HomeScreen(),
     ),
 
     GoRoute(
       path: '/buttons',
+      name: ButtonsScreen.name,
       builder: (context, state) => const ButtonsScreen(),
     ),
 
+    // GoRoute(
+    //   path: '/cards',
+    //   name: CardsScreen.name,
+    //   builder: (context, state) => const CardsScreen(),
+    // ),
+
     GoRoute(
       path: '/cards',
-      builder: (context, state) => const CardsScreen(),
+      name: CardsScreen.name,
+      pageBuilder: (context, state) {
+        return CustomTransitionPage(
+          key: state.pageKey,
+          child: const CardsScreen(),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            // Change the opacity of the screen using a Curve based on the the animation's
+            // value
+            return FadeTransition(
+              opacity:
+                  CurveTween(curve: Curves.easeInOutCirc).animate(animation),
+              child: child,
+            );
+          },
+        );
+      },
     ),
   ],
 );
