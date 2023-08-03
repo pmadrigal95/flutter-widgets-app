3a8dd73979ce27439932335a53e21c78d272eed3
Pablo Madrigal <pablomadrigal.1995@gmail.com>
2023-08-02 19:22:35 -0600

    images add

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -52,4 +52,10 @@ const appMenuItems = <MenuItem>[
     subTitle: 'Una serie de controles de Flutter',
     link: '/ui-controls',
     icon: Icons.text_snippet_outlined),
+
+  MenuItem(
+    title: 'Introducción a la aplicación',
+    subTitle: 'Pequeño tutorial introductorio',
+    link: '/tutorial',
+    icon: Icons.sports_esports_rounded),
 ];
--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -68,5 +68,12 @@ final appRouter = GoRouter(
       name: UiControlsScreen.name,
       builder: (context, state) => const UiControlsScreen(),
     ),
+
+
+    GoRoute(
+      path: '/tutorial',
+      name: AppTutorialScreen.name,
+      builder: (context, state) => const AppTutorialScreen(),
+    ),
   ],
 );
